08f73488204af1632e33ca80a99e384fe54ea1eb
mateusDevFront <mateusdsgn2001@gmail.com>
2024-02-25 14:08:48 -0300

    ft connect api-openai

--- App.tsx
+++ App.tsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Pressable,
+  ScrollView,
+  ActivityIndicator
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 import Slider from "@react-native-community/slider";
 
 export default function App() {
+
+  const [city, setCity] = useState("")
+  const [days, setDays] = useState(1)
+  const [loading, setLoading] = useState(false);
+  const [travel, setTravel] = useState("");
+
   return (
     <View style={styles.container}>
       <StatusBar translucent={true} backgroundColor="#f1f1f1" />
@@ -11,19 +27,45 @@ export default function App() {
 
       <View style={styles.form}>
         <Text style={styles.label}>Destino</Text>
-        <TextInput placeholder="EX: Fortaleza - CE" style={styles.input} />
+        <TextInput
+          value={city}
+          onChangeText={(text) => setCity(text)}
+        placeholder="EX: Fortaleza - CE" style={styles.input} />
 
         <Text style={styles.label}>
-          Tempo de estadia: <Text style={styles.days}>10</Text> dias{" "}
+          Tempo de estadia: <Text style={styles.days}>{days.toFixed(0)}</Text> dias{" "}
         </Text>
 
         <Slider
+          value={days}
+          onValueChange={(value) => setDays(value)}
           minimumValue={1}
           maximumValue={7}
           minimumTrackTintColor="#009698"
           maximumTrackTintColor="#000000"
         />
       </View>
+
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Gerar o roteiro</Text>
+        <MaterialIcons name="travel-explore" size={24} color="#FFF" />
+      </Pressable>
+
+      <ScrollView style={styles.contentScroll}>
+        {loading && (
+          <View style={styles.content}>
+            <Text style={styles.title}>Roteiro da sua viagem</Text>
+            <ActivityIndicator  color="#000000" size="large"/>
+            <Text>{travel}</Text>
+          </View>
+        )}
+        {travel && (
+          <View style={styles.content}>
+            <Text style={styles.title}>Carregando...</Text>
+            <Text>{travel}</Text>
+          </View>
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -63,5 +105,37 @@ const styles = StyleSheet.create({
   },
   days: {
     backgroundColor: "#f1f1f1",
+  },
+  button: {
+    backgroundColor: "#ff5656",
+    width: "90%",
+    borderRadius: 8,
+    flexDirection: "row",
+    padding: 14,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 8,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  content: {
+    backgroundColor: "#fff",
+    padding: 16,
+    width: "100%",
+    marginTop: 16,
+    borderRadius: 8,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 14,
+  },
+  contentScroll: {
+    width: "90%",
+    marginTop: 8,
   },
 });
